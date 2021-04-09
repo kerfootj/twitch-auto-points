b62b84447bd9ac06c8eaa869daf2055d66dfbe48
Joel Kerfoot <joelkerfoot@gmail.com>
2021-04-08 19:51:30 -0700

    use MutationObserver instead of setTimeout

--- points.js
+++ points.js
@@ -1,15 +1,29 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = sec => new Promise(resolve => setTimeout(resolve, sec * 1000));
 
 const collect = async () => {
-  for (;;) {
-    const elements = document.getElementsByClassName("claimable-bonus__icon")
-    if (elements.length) {
-      elements[0].click()
-      await sleep(60000)
-    } else {
-      await sleep(2000)
-    }
-  }
-}
+    await sleep(30);
 
+    // the point container
+    const target = document.getElementsByClassName("community-points-summary")[0];
+    
+    // create a new mutation observer
+    const observer = new MutationObserver(() => {
+        // check if the points button exists
+        const element = document.getElementsByClassName("claimable-bonus__icon")[0];
+
+        if (element) {
+            // click the claim bonus button
+            element.click();
+        }
+        
+    });
+    
+    // disconnect the observer when the page unloads
+    window.onbeforeunload = () => observer.disconnect();
+    
+    // start observing changes
+    observer.observe(target, { subtree: true, childList: true });
+};
+
+// collect points
 collect();
